06ecea0d6d49ad43e0e95494a8238c08b241592a
sniki-ld <dennisalenaf@gmail.com>
2022-02-07 10:38:53 +0300

    внесены правки

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -38,14 +38,14 @@ class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
-        # else:
-        #     return request.user.is_authenticated and request.user.is_admin
+        else:
+            return request.user.is_authenticated and request.user.is_admin
 
     def has_object_permission(self, request, view, obj):
         return request.method in permissions.SAFE_METHODS or (
                 request.user.is_authenticated and request.user.is_admin)
 
-      
+
 class ReadOnlyOrAuthorOrAdmin(permissions.BasePermission):
     """Доступ на чтение для всех. Изменение - только авторам/админам."""
 
